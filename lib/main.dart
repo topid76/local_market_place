b72fa200b890e9174d70ad08626d9ee5f000db20
Jia Labuga <jaykim>
2023-09-08 16:12:07 +0800

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       onGenerateRoute: generateRoute,
-      initialRoute: RegistrationRoute,
+      initialRoute: SellerRegistrationRoute,
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
--- lib/screens/seller_registration_page.dart
+++ lib/screens/seller_registration_page.dart
@@ -6,16 +6,19 @@ class SellerRegistrationPage extends StatelessWidget {
     return (Scaffold(
       appBar: AppBar(
         leading: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.close,
-              size: 30,
-              color: Colors.black,
-            )),
+          onPressed: () {},
+          icon: Icon(
+            Icons.close,
+            size: 30,
+            color: Colors.black,
+          ),
+        ),
+        title: Text("Seller Registration"),
       ),
       body: Column(children: [
+        Padding(padding: EdgeInsets.fromLTRB(0, 50, 0, 0)),
         Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             SizedBox(
               width: 350,
@@ -24,6 +27,101 @@ class SellerRegistrationPage extends StatelessWidget {
                 decoration: InputDecoration(
                     border: OutlineInputBorder(), labelText: "Shop Name"),
               ),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              width: 350,
+              child: TextField(
+                obscureText: false,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Pick up Address"),
+              ),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              width: 350,
+              child: TextField(
+                obscureText: false,
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Detailed Address"),
+              ),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              width: 350,
+              child: TextField(
+                obscureText: false,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Email"),
+              ),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              width: 350,
+              child: TextField(
+                obscureText: false,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Phone Number"),
+              ),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                    style: TextButton.styleFrom(
+                      fixedSize: const Size(300, 40),
+                      backgroundColor: Colors.blueAccent,
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      "Submit",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 15),
+                    ))
+              ],
             )
           ],
         )
