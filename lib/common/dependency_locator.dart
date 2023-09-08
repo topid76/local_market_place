995986402f2f5ce0c10317a412e5fb3b16ca1dd8
jeff <jefferson@edamama.ph>
2023-09-08 16:29:17 +0800

    added app provider

--- lib/common/dependency_locator.dart
+++ lib/common/dependency_locator.dart
@@ -1,5 +1,8 @@
 import 'package:get_it/get_it.dart';
+import 'package:local_marketplace/notifiers/app_notifier.dart';
 
 GetIt getIt = GetIt.instance;
 
-void setupLocator() {}
+void setupLocator() {
+  getIt.registerLazySingleton<AppNotifier>(() => AppNotifier());
+}
